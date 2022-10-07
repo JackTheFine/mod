447ea85d988c53bb1cc56f763ea0d11b814c16e2
Jack Maradon <112029550+JackTheFine@users.noreply.github.com>
2022-10-07 04:20:33 +0000

    Added ignore

--- README.md
+++
@@ -1,4 +0,0 @@
-# Mod
-This is a discord bot made for a virtual flight game guild and not to be used for anything else, it can be sourced if you would like but I don't really care! Just make sure to setup config.json since I have it ignored.
-## Author
-Me and xei (a spa manager)
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
-}//hi
+}
 
 client.once('ready', () => {
   console.log('Ready!');
